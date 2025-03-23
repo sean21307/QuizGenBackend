28ed5cf5cd908be5c3c53eaaefb9bef7bfd495a9
Sean Nolan <seannolan21307@gmail.com>
2025-03-22 20:43:30 -0400

    init

---
+++ src/main/java/com/example/quizgenbackend/QuizGenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizgenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizGenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quizgenbackend/QuizGenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizgenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizGenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
